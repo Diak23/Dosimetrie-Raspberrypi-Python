7e0e8184e375f7b9b75a665052c2066aa6822976
Diak23 <diakllo23@gmail.com>
2026-05-11 13:09:10 +0200

    Ajout mesures sauvegarde csv

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from acquisition.capteur import lecture_capteur
 from processing.exposition import mesurer_exposition
+from processing.stockage import sauvegarder_mesure
 
 print("Démarrage du système TEMPO")
 
 valeur = lecture_capteur()
 
 if valeur > 3:
+    statut = "Exposition élevée"
     print("⚠️ Niveau d'exposition élevé")
 else:
+    statut = "Niveau normal"
     print("✅ Niveau normal")
 
+sauvegarder_mesure(valeur, statut)
 mesurer_exposition(5)
